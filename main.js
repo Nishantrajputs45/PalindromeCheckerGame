8fc2c1107b3499ac23fec306c37a8d06f566513f
Nshantrajputs45 <nishantrajputs45@gmail.com>
2022-10-07 06:54:01 +0530

    Modified the reguler expression

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const result = document.querySelector(".result");
 
 function countWord() {
   const regex = /\s+/gi;
-  const userText = userInput.value.trim().replace(regex, " ").toLowerCase();
+  const userText = userInput.value.trim().replace(regex, "").toLowerCase();
   const testText = userText
     .split("")
     .map((word) => word.split("").reverse())
