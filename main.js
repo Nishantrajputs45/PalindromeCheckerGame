106b5bcf17b36b8fe841e3843ce351b7fa5957ae
Nshantrajputs45 <nishantrajputs45@gmail.com>
2022-10-07 06:48:51 +0530

    Logic Added for Checking Palindrome

---
+++ main.js
@@ -0,0 +1,17 @@
+const userInput = document.querySelector("#input");
+const result = document.querySelector(".result");
+
+function countWord() {
+  const regex = /\s+/gi;
+  const userText = userInput.value.trim().replace(regex, " ").toLowerCase();
+  const testText = userText
+    .split("")
+    .map((word) => word.split("").reverse())
+    .reverse()
+    .join("");
+  if (userText === testText) {
+    result.innerText = "Yes, It Is a Palindrome";
+  } else {
+    result.innerText = "No., It Is not a Palindrome";
+  }
+}
